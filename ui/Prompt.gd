f7759cb8b1fb0cb4e42bf4e5fab7031139557aa6
Drakmyth <shaun.hamman@gmail.com>
2023-09-09 09:17:38 -0700

    Wrap text_submitted signal in custom command_submitted signal

--- ui/Prompt.gd
+++ ui/Prompt.gd
@@ -1,5 +1,12 @@
 extends HBoxContainer
 
+signal command_submitted
 
 func _ready() -> void:
+	$PromptInput.connect("text_submitted", _on_PromptInput_text_submitted)
+	
 	$PromptInput.grab_focus()
+
+func _on_PromptInput_text_submitted(new_text: String) -> void:
+	command_submitted.emit(new_text)
+	$PromptInput.clear()
