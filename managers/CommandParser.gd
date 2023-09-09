c5ec140b0b45d44636a4f4645adc20e893b70133
Drakmyth <shaun.hamman@gmail.com>
2023-09-09 09:43:17 -0700

    Skip buzzwords during parsing

--- managers/CommandParser.gd
+++ managers/CommandParser.gd
@@ -7,15 +7,19 @@ func parse_command(input: String) -> Array[Command]:
 
 	print("Raw input: %s" % input)
 
+	# Wrap spaces around punctuation that starts a new clause
 	var punctuation_tokens = [".", ",", "\""]
 	for token in punctuation_tokens:
 		input = input.replace(token, " %s " % token)
 
+	# Tokenize input and replace synonyms
 	var tokens = Array(input.split(" ", KEEP_EMPTY_TOKENS)).map(func(word): return Vocabulary.resolve(word))
 
 	print("Processed input: %s" % " ".join(tokens))
+
+	# No input, no command
 	if len(tokens) == 0:
-		print("I beg your pardon?\n")
+		print("I beg your pardon?")
 		return []
 
 	var commands = [] as Array[Command]
@@ -23,18 +27,25 @@ func parse_command(input: String) -> Array[Command]:
 
 	var words_remaining = len(tokens)
 
+	# Parse input word by word
 	for ptr in range(len(tokens)):
-		var word = Vocabulary.resolve(tokens[ptr])
+		var word = tokens[ptr]
 		words_remaining -= 1
-		var next_word = Vocabulary.resolve(tokens[ptr + 1]) if words_remaining > 0 else ""
+		var next_word = tokens[ptr + 1] if words_remaining > 0 else ""
 
+		# Buzzwords are either skipped entirely or have special handling where relevant
+		if Vocabulary.is_part_of_speech(word, Vocabulary.PartOfSpeech.BUZZWORD):
+			continue
+
+		# Moving around may not provide a verb, so process movement first
 		if _check_walk(command, word, next_word):
-			command.verb = "walk"
+			command.verb = "walk" # This may override the verb, but only if it's already "walk" so it's fine
 			command.object1 = word
 			print("Command: %s" % command.as_phrase())
 			commands.append(command)
 			if next_word == "then":
 				command = Command.new()
+		# All other commands require a verb to be specified
 		elif Vocabulary.is_part_of_speech(word, Vocabulary.PartOfSpeech.VERB):
 			command.verb = word
 	return commands
