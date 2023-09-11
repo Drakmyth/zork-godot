3dca6a116bb77eb58ba7147d1c5541233cfdec95
Drakmyth <shaun.hamman@gmail.com>
2023-09-10 18:09:35 -0700

    Update Room template to match latest script structure

--- script_templates/Room/Default.gd
+++ script_templates/Room/Default.gd
@@ -1,5 +1,14 @@
 # meta-default: true
 extends Room
 
-func action() -> void:
+func describe_tokens() -> Array:
+	return []
+
+func on_enter() -> void:
 	pass
+
+func on_begin_command() -> String:
+	return ""
+
+func on_end_command() -> String:
+	return ""
