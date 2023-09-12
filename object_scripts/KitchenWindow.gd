12d5f23ebf4789207a830c5846fb05ad526d372b
Drakmyth <shaun.hamman@gmail.com>
2023-09-11 22:07:54 -0700

    Implement remaining Kitchen Window logic

--- object_scripts/KitchenWindow.gd
+++ object_scripts/KitchenWindow.gd
@@ -1,44 +1,40 @@
 extends Door
 
-func _first_description_tokens() -> Array:
-	return []
-
-func _floor_description_tokens() -> Array:
-	return [description] if floor_description.is_empty() else []
-
 func action(command: Command, player: Player) -> String:
 	match(command.verb):
-		"open":
-			return _handle_open(command, player)
-		"close":
-			return _handle_close(command, player)
-		"examine":
-			return _handle_examine(command, player)
-		"walk":
-			return _handle_walk(command, player)
-	
-	if command.verb == "look" and ["with", "in"].has(command.preposition):
-		return _handle_look_in(command, player)
+		Vocabulary.Verbs.OPEN:
+			return _handle_open_close(command)
+		Vocabulary.Verbs.CLOSE:
+			return _handle_open_close(command)
+		Vocabulary.Verbs.EXAMINE:
+			return "The window is slightly ajar, but not enough to allow entry." if parser_flags & Thing.FLAG_TOUCHED else ""
+		Vocabulary.Verbs.WALK:
+			return _handle_walk(player)
+		Vocabulary.Verbs.BOARD:
+			return _handle_walk(player)
+		Vocabulary.Verbs.ENTER:
+			return _handle_walk(player)
+		Vocabulary.Verbs.LOOK:
+			return _handle_look(command, player)
 
 	return ""
 
-func _handle_open(_command:Command, _player: Player) -> String:
-	if open: return Vocabulary.get_random_dummy_response()
+func _handle_open_close(command:Command) -> String:
+	parser_flags &= Thing.FLAG_TOUCHED
+	if command.verb == Vocabulary.Verbs.OPEN and open: return Vocabulary.get_random_dummy_response()
+	if command.verb == Vocabulary.Verbs.CLOSE and not open: return Vocabulary.get_random_dummy_response()
 
-	open = true
-	return "With great effort, you open the window far enough to allow entry."
+	open = command.verb == Vocabulary.Verbs.OPEN
+	return "With great effort, you open the window far enough to allow entry." if open \
+		else "The window closes (more easily than it opened)."
 
-func _handle_close(_command:Command, _player: Player) -> String:
-	if not open: return Vocabulary.get_random_dummy_response()
+func _handle_walk(player: Player) -> String:
+	return player.move(Vocabulary.Directions.EAST) if player.get_room().name == "kitchen" else player.move(Vocabulary.Directions.WEST)
 
-	open = false
-	return "The window closes (more easily than it opened)."
+func _handle_look(command:Command, player: Player) -> String:
+	if not command.preposition == Vocabulary.Prepositions.IN: return ""
 
-func _handle_examine(_command:Command, _player: Player) -> String:
-	return ""
-
-func _handle_walk(_command:Command, _player: Player) -> String:
-	return ""
-
-func _handle_look_in(_command:Command, _player: Player) -> String:
-	return ""
+	if player.get_room().name == "kitchen":
+		return "You can see a clear area leading towards a forest."
+	else:
+		return "You can see what appears to be a kitchen."
