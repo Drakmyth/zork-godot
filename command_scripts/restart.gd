6f0b7cba1bd8d38ecd13825e45a770bbf27cb154
Drakmyth <shaun.hamman@gmail.com>
2023-09-22 10:56:17 -0700

    Implement 'restart' command.

--- command_scripts/restart.gd
+++ command_scripts/restart.gd
@@ -1,4 +1,13 @@
 extends Command
 
-func action(_command: Command, _player: Player) -> String:
-	return "Restarting..."
+func action(_command: Command, player: Player) -> String:
+	var message = \
+"Your score is %d (total of %d points), in %d moves.
+This gives you the rank of %s.
+Do you wish to restart?" % [player.score, Player.SCORE_MAX, player.moves, player.get_rank()]
+
+	var confirmed = await DialogManager.confirm(message)
+	if confirmed:
+		player.get_tree().reload_current_scene()
+
+	return "Ok."
